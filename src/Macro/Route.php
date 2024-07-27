9655a82c5ebf669ea0ecdc24db0b31ab1ece0621
Darkking1098 <ajaykumarraven147@gmail.com>
2024-07-27 14:24:58 +0530

    Api Rotes macro added

--- src/Macro/Route.php
+++ src/Macro/Route.php
@@ -2,8 +2,35 @@
 
 use Illuminate\Support\Facades\Route;
 
+function render_routes($defaultRoutes, $name, $controller, array $config = [])
+{
+    if (isset($config['only'])) {
+        $defaultRoutes = array_intersect_key($defaultRoutes, array_flip($config['only']));
+    }
+    if (isset($config['except'])) {
+        $defaultRoutes = array_diff_key($defaultRoutes, array_flip($config['except']));
+    }
+
+    foreach ($config['routes'] ?? [] as $key => $routes) {
+        $defaultRoutes[$key] = array_merge($defaultRoutes[$key] ?? [], $routes);
+    }
+
+    $view_route = ($config['name'] ?? $name) . ".view_all";
+
+    Route::controller($controller)->prefix($name)->name($config['name'] ?? $name)->group(function () use ($defaultRoutes, $view_route) {
+        foreach ($defaultRoutes as $controllerMethod => $method_config) {
+            foreach ($method_config as $routeName => [$method, $uri]) {
+                Route::$method($uri, $controllerMethod)->name(".$routeName");
+            }
+        }
+        Route::get('', function () use ($view_route) {
+            return redirect()->route($view_route);
+        });
+    });
+}
+
 Route::macro('MantisWebResource', function ($name, $controller, array $config = []) {
-    $defaultRoutes = [
+    render_routes([
         'ui_modify' => [
             "create" => ['get', 'create'],
             "update" => ['get', 'update/{id}']
@@ -27,24 +54,29 @@ Route::macro('MantisWebResource', function ($name, $controller, array $config = 
             "delete" => ['get', 'delete/{id}'],
             "delete.delete" => ['delete', 'delete/{id}'],
         ],
-    ];
+    ], $name, $controller, $config ?? []);
+});
 
-    if (isset($config['only'])) {
-        $defaultRoutes = array_intersect_key($defaultRoutes, array_flip($config['only']));
-    }
-    if (isset($config['except'])) {
-        $defaultRoutes = array_diff_key($defaultRoutes, array_flip($config['except']));
-    }
-
-    foreach ($config['routes'] ?? [] as $key => $routes) {
-        $defaultRoutes[$key] = array_merge($defaultRoutes[$key] ?? [], $routes);
-    }
-
-    Route::controller($controller)->prefix($name)->name($config['name'] ?? $name)->group(function () use ($defaultRoutes) {
-        foreach ($defaultRoutes as $controllerMethod => $method_config) {
-            foreach ($method_config as $routeName => [$method, $uri]) {
-                Route::$method($uri, $controllerMethod)->name(".$routeName");
-            }
-        }
-    });
+Route::macro('MantisApiResource', function ($name, $controller, array $config = []) {
+    render_routes([
+        'api_view' => [
+            "view_all" => ['get', 'view'],
+            "view" => ['get', 'view/{id}'],
+        ],
+        'api_modify' => [
+            "create" => ['post', 'create'],
+            "update" => ['post', 'update/{id}'],
+            "update.put" => ['put', 'update/{id}'],
+            "update.patch" => ['patch', 'update/{id}']
+        ],
+        'api_toggle' => [
+            "toggle" => ['get', 'toggle/{id}'],
+            "toggle.put" => ['put', 'toggle/{id}'],
+            "toggle.patch" => ['patch', 'toggle/{id}'],
+        ],
+        'api_delete' => [
+            "delete" => ['get', 'delete/{id}'],
+            "delete.delete" => ['delete', 'delete/{id}'],
+        ],
+    ], $name, $controller, $config ?? []);
 });
